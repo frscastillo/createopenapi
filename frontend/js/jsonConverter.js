c53f7d745ba247ba5c17bd5a967352170b95246c
Franco <manifestador@gmail.com>
2025-11-13 00:08:24 -0300

    Delete frontend directory

--- frontend/js/jsonConverter.js
+++
@@ -1,6 +0,0 @@
-// Shim re-export to legacy implementation (fixed and moved to js/legacy)
-import { validateJSON, jsonToOpenAPI } from './legacy/jsonConverter.js';
-
-export { validateJSON, jsonToOpenAPI };
-
-export default { validateJSON, jsonToOpenAPI };
